329885a121cca3cff57b8b4bdde5d29af627f5d3
Leonardo <leonardodlima10@hotmail.com>
2022-10-12 17:22:39 -0300

    criação(Application): Criada as ViewModels com automapper para produto endereco e fornecedor

---
+++ src/AppTresCamadas.Application/ViewModels/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AppTresCamadas.Business.Models;
+using AutoMapper;
+
+namespace AppTresCamadas.Application.ViewModels.AutoMapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<FornecedorModel, FornecedorViewModel>().ReverseMap();
+            CreateMap<EnderecoModel, EnderecoViewModel>().ReverseMap();
+            CreateMap<ProdutoModel, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
